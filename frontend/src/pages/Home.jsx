c2eba5bf872feb488f7478547b3f973ec895cf9f
fmordy01 <fmordych@gmail.com>
2024-04-15 23:01:51 +0100

    Home.jsx: Bulding the home page, added functionality to display notes written by the user

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,24 @@
+import { useState, useEffect } from "react";
+import api from "../api";
+
 function Home() {
-    return <div>Home</div>
+    const [notes, setNotes] = useState([]);
+    const [content, setContent] = useState("");
+    const [title, setTitle] = useState("");
+
+    useEffect(() => {
+        getNotes();
+    }, [])
+
+    const getNotes = () => {
+        api
+        .get("/api/notes/")
+        .then((res) => res.data)
+        .then((data) => { setNotes(data); console.log(data) })
+        .catch((err) => alert(err));
+    };
+
+    return <div>Home</div>;
 }
 
-export default Home
+export default Home;
